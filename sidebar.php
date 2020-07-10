424766e9c2644a2b5ef595141d0de6bce49bbfa5
Ishant Sitaula <changingmission@gmail.com>
2020-07-10 11:52:25 +0545

    fb width

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <?php dynamic_sidebar('sidebar-1');?>
-<div class="fb-page" data-href="https://www.facebook.com/ishant.sitaula" data-tabs="" data-width="" data-height=""
+<div class="fb-page" data-href="https://www.facebook.com/ishant.sitaula" data-tabs="" data-width="500px" data-height=""
   data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
   <blockquote cite="https://www.facebook.com/ishant.sitaula" class="fb-xfbml-parse-ignore"><a
       href="https://www.facebook.com/ishant.sitaula">Ishant</a></blockquote>
